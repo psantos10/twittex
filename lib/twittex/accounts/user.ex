f11cd9f4e04387aac916370c978021e82eb1ef82
Patrício dos Santos <hello@psantos.dev>
2024-09-19 19:55:22 +0100

    add users.username column

--- lib/twittex/accounts/user.ex
+++ lib/twittex/accounts/user.ex
@@ -4,6 +4,7 @@ defmodule Twittex.Accounts.User do
 
   schema "users" do
     field :email, :string
+    field :username, :string
     field :password, :string, virtual: true, redact: true
     field :hashed_password, :string, redact: true
     field :current_password, :string, virtual: true, redact: true
