c86a8816c1d41481fd8a0611b98bdca370124a5d
Patrício dos Santos <hello@psantos.dev>
2024-09-21 15:33:38 +0100

    add non-functional form to profile page

--- lib/twittex_web/live/profile_live.ex
+++ lib/twittex_web/live/profile_live.ex
@@ -5,15 +5,20 @@ defmodule TwittexWeb.ProfileLive do
 
   alias Twittex.Accounts
   alias Twittex.Feed
+  alias Twittex.Feed.Tweek
+
+  on_mount {TwittexWeb.UserAuth, :mount_current_user}
 
   def mount(%{"username" => username}, _session, socket) do
     user = Accounts.get_user_by_username!(username)
     tweeks = Feed.list_tweeks_for_user(user)
+    form = Feed.change_tweek(%Tweek{}) |> to_form()
 
     socket =
       socket
       |> assign(:user, user)
       |> assign(:tweeks, tweeks)
+      |> assign(:form, form)
 
     {:ok, socket}
   end
