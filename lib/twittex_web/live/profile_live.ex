5d581b95cc25b1fbf5dd28093adcb09c0406ca3f
Patrício dos Santos <hello@psantos.dev>
2024-09-21 13:59:23 +0100

    list tweeks on profile

--- lib/twittex_web/live/profile_live.ex
+++ lib/twittex_web/live/profile_live.ex
@@ -1,10 +1,20 @@
 defmodule TwittexWeb.ProfileLive do
   use TwittexWeb, :live_view
 
+  import TwittexWeb.FeedComponents
+
   alias Twittex.Accounts
+  alias Twittex.Feed
 
   def mount(%{"username" => username}, _session, socket) do
     user = Accounts.get_user_by_username!(username)
-    {:ok, assign(socket, :user, user)}
+    tweeks = Feed.list_tweeks_for_user(user)
+
+    socket =
+      socket
+      |> assign(:user, user)
+      |> assign(:tweeks, tweeks)
+
+    {:ok, socket}
   end
 end
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,19 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Twittex.Accounts
+alias Twittex.Feed
+
+user = Accounts.get_user_by_username!("phoenixonrails")
+
+[
+  "Phoenix rocks!",
+  "just setting up my twittx",
+  "Despite the constant negative press covfefe",
+  "If only Bradley's arm was longer",
+  "All Bitcoin sent to the address below will be sent back doubled!"
+]
+|> Enum.each(fn content ->
+  {:ok, _user} = Feed.create_tweek_for_user(user, %{content: content})
+end)
